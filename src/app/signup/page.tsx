95ccf8c16b0dbc1172ad283e3dd44e17becb360b
M Dawood Javed <156129012+dawoodjaved5@users.noreply.github.com>
2025-06-05 22:56:23 +0500

    final

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -99,12 +99,10 @@ export default function LoginPage() {
   async function handleGoogleLogin() {
     try {
       setLoading(true);
-      const redirectSuccess = process.env.NEXT_PUBLIC_REDIRECT_SUCCESS!;
-      const redirectFail = process.env.NEXT_PUBLIC_REDIRECT_FAIL!;
       await account.createOAuth2Session(
-        OAuthProvider.Google,
-        redirectSuccess,
-        redirectFail
+        OAuthProvider.Google,// Using string "google" as per your preference
+        "https://car-share-alpha/dashboard",
+        "https://car-share-alpha/login"
       );
       console.log("Google login initiated");
     } catch (error: any) {
